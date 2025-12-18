93f02f35cb102903b4139cfc5cda113c87168312
fortavey <fortavey@gmail.com>
2025-12-18 12:31:34 +0300

    Initial Commit

---
+++ Brends/BrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrendsApp.swift
+//  Brends
+//
+//  Created by Main on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
